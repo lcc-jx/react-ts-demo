78414ccfe7719193e6db134de0489cbc40f85757
v_liuchencheng <v_liuchencheng@baidu.com>
2024-09-24 17:23:54 +0800

    test:账户名

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 // 语义：
 function App() {
   const questionList = [
-    { id: 'q1', title: '问卷1', isPublished: false },
+    { id: 'q1', title: '问卷1', isPublished: true },
     { id: 'q2', title: '问卷2', isPublished: false },
-    { id: 'q3', title: '问卷3', isPublished: false },
+    { id: 'q3', title: '问卷3', isPublished: true },
     { id: 'q4', title: '问卷4', isPublished: false },
   ]
   function editQuestion(id: string) {
